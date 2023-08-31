842e2370b728ef81b59baa5945e1265a5993f756
KellyKiiru <infowithkiiru@gmail.com>
2023-09-01 02:07:17 +0300

    changes

--- imageUpload/settings.py
+++ imageUpload/settings.py
@@ -25,7 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'upload'
+    'django.contrib.sites',
+    'upload',
+
 ]
 
 MIDDLEWARE = [
