eff3e4f2857c1c10752cbd7829183dad2026f894
KellyKiiru <infowithkiiru@gmail.com>
2023-09-01 03:34:59 +0300

    install django-cors-headers and set CORS_ALLOW_ALL_ORIGINS = True

--- imageUpload/settings.py
+++ imageUpload/settings.py
@@ -25,9 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
+    'rest_framework',
     'upload',
-
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -112,3 +112,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
