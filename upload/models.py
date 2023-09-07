29a2566967a2c4fece985e8b59f5b40f8419232f
KellyKiiru <infowithkiiru@gmail.com>
2023-09-07 18:26:47 +0300

    model field change

--- upload/models.py
+++ upload/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Image(models.Model):
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=50)
-    image = models.BinaryField()
+    image = models.ImageField()
 
     def __str__(self):
         return self.name
