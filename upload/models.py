76767351b17f3ebb7a51c4020c38178f81188953
kellykiiru <infowithkiiru@gmail.com>
2024-01-24 21:19:21 +0300

    delete wrong code

--- upload/models.py
+++ upload/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Image(models.Model):
     name = models.CharField(max_length=255)
-    type = models.CharField(max_length=50)
     image = models.ImageField()
 
     def __str__(self):
